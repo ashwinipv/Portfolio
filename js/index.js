e26ec48753e549bbc2362f0d3a2a94e8ed4c80c1
ashwinipv <ashwinipv@gmail.com>
2022-02-15 21:41:38 -0500

    added hamburger menu

--- js/index.js
+++ js/index.js
@@ -2,3 +2,14 @@ function fade() {
     $(".preloader").fadeOut("slow");
 }
 setTimeout(fade, 3000)
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
