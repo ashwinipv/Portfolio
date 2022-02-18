e1744a7032999b9a43c69bc628e6931cb8772bd3
ashwinipv <ashwinipv@gmail.com>
2022-02-17 23:40:18 -0500

    hamburger menu added to mobile
    but unable to fix alignment

--- js/index.js
+++ js/index.js
@@ -3,13 +3,33 @@ function fade() {
 }
 setTimeout(fade, 3000)
 
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+/* function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
-}
+} 
 
+function myFunction() {
+  var x = document.getElementById("displayNav");
+  if (x.style.display === "block") {
+   x.style.display = "none";
+ } else {
+    x.style.display = "block";
+ }
+} */
+
+
+function myFunction() {
+  var x = document.getElementById("myid");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
